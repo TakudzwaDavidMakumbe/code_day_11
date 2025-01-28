294db6d18eb03ffba6ed2247eec2912ef435a670
TakudzwaDavidMakumbe <takuclarity@gmail.com>
2025-01-28 14:54:33 +0200

    refactor Hero and Navbar components for improved responsiveness and layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,10 @@ import hero from '../assets/Group 11.png'
 
 const Hero = () => {
   return (
-    <div className='flex-col py-10 justify-center  text-white' >
+    <div className='flex-col max-sm:justify-center max-sm:items-center max-sm:mt-12 py-10 justify-center  text-white' >
         <div className=" w-[80%] mx-auto">
             <div className="   pl-[6rem] text-center flex justify-center items-center">
-                <h1 className="text-8xl   tracking-[2rem] text-gray-200/10 font-extrabold">
+                <h1 className="text-8xl max-sm:text-5xl max-sm:pr-10   tracking-[2rem] text-gray-200/10 font-extrabold">
                     PILOT JOBS
                 </h1>
             </div>
@@ -16,11 +16,11 @@ const Hero = () => {
         <div className="flex justify-center">
              <img src={hero1} alt="" className="w-[70%]" />
         </div>
-        <div className=" flex-col py-3 text-center justify-center items-center">
-            <h1 className="text-white  font-medium text-[2.5rem]">
+        <div className=" flex-col  py-3 text-center justify-center items-center">
+            <h1 className="text-white max-sm:mb-1 max-sm:text-xl font-medium text-[2.5rem]">
                 Travel Around The World
             </h1>
-            <p className="text-white">One of the advantages of being disorganized is that one is always having suprising discoveries</p>
+            <p className="text-white max-sm:px-5 max-sm:text-sm">One of the advantages of being disorganized is that one is always having suprising discoveries</p>
         </div>
 
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,12 @@ import Hero from './Hero'
 
 const Navbar = () => {
   return (
-    <div className='flex'>
-        <div className="flex py-5 px-10 justify-between w-full" >
+    <div className='flex w-screen overflow-hidden'>
+        <div className="flex py-5 px-10 justify-between w-screen" >
            <div className="">
            <img src={logo} alt="logo" className=" "/>
            </div>
-            <div className="flex justify-center   px-5">
+            <div className="hidden md:flex justify-center   px-5">
                 <div className="">
                 <a href="#" className="p-4  text-white hover:text-blue-400">All Flight</a>
                 <a href="#" className="p-4  text-white hover:text-blue-400">Schedule</a>
@@ -21,7 +21,29 @@ const Navbar = () => {
                 <a href="#" className="px-5 py-2  text-white hover:bg-white hover:font-bold hover:text-blue-400 rounded-full border">Let`s Fly</a> 
                 </div>
             </div>
-            
+            {/* --humberger--- */}
+            <button className="space-y-1 group md:hidden">
+              <div className="w-6 h-1 bg-white"></div>
+              <div className="w-6 h-1 bg-white"></div>
+              <div className="w-6 h-1 bg-white"></div>
+            </button>
+
+            {/* --menu--- */}
+            <div className="bg-[#252525] w-screen pb-10 absolute -top-full group-focus:top-full right-0 duration-150 flex flex-col space-y-3 justify-end">
+              <button className="px-10 py-8 relative ml-auto">
+                <div className="w-6 h-1 rotate-45 absolute bg-white"></div>
+                <div className="w-6 h-1 -rotate-45 absolute bg-white"></div>
+              </button>
+                <div className="">
+                <a href="#" className="py-4  flex justify-center w-full text-white hover:bg-[#202020]">All Flight</a>
+                <a href="#" className="py-4  flex justify-center w-full text-white hover:bg-[#202020]">Schedule</a>
+                <a href="#" className="py-4  flex justify-center w-full text-white hover:bg-[#202020]">Passangers</a>
+                <a href="#" className="py-4  flex justify-center w-full text-white hover:bg-[#202020]">Your Orders</a> 
+                </div>
+                <div className="">
+                <a href="#" className="py-4  flex justify-center w-full  text-white hover:bg-white hover:font-bold hover:text-blue-400 rounded-full border">Let`s Fly</a> 
+                </div>
+            </div>
         </div>
         
     </div>
